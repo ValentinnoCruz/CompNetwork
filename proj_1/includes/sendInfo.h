60c05b2da37a504e1b54549a1e877570eca6e3f1
valentinn cruz <valentinnocruz@gmail.com>
2022-03-17 11:11:55 -0800

    added all include header files

---
+++ proj_1/includes/sendInfo.h
@@ -0,0 +1,17 @@
+
+#ifndef PACK_BUFFER_H
+#define PACK_BUFFER_H
+
+#include "packet.h"
+
+enum{
+	SEND_BUFFER_SIZE=128
+};
+
+typedef struct sendInfo{
+	pack packet;
+	uint16_t src;
+	uint16_t dest;
+}sendInfo;
+
+#endif /* PACK_BUFFER_H */
